dc36b573ad1b67fa9604c398e8e8fa4e99383016
Helge Heß <devteam@zeezide.de>
2024-09-11 18:21:56 +0200

    Initial Fomantic-UI 2.9.3
    Nice.

---
+++ Sources/FomanticUI/ResourceMap.swift
@@ -0,0 +1,426 @@
+// Generated on Wed Sep 11 18:19:43 CEST 2024
+//
+
+import struct Foundation.Data
+
+public extension FomanticUI {
+
+  /**
+   * Lookup a FomanticUI resource. The returned Data
+   * contains the gzip compressed resource.
+   *
+   * Available resource names:
+   *
+   * - "semantic.min.css"
+   * - "semantic.min.js"
+   * - "components/accordion.min.css"
+   * - "components/ad.min.css"
+   * - "components/breadcrumb.min.css"
+   * - "components/button.min.css"
+   * - "components/calendar.min.css"
+   * - "components/card.min.css"
+   * - "components/checkbox.min.css"
+   * - "components/comment.min.css"
+   * - "components/container.min.css"
+   * - "components/dimmer.min.css"
+   * - "components/divider.min.css"
+   * - "components/dropdown.min.css"
+   * - "components/embed.min.css"
+   * - "components/emoji.min.css"
+   * - "components/feed.min.css"
+   * - "components/flag.min.css"
+   * - "components/flyout.min.css"
+   * - "components/form.min.css"
+   * - "components/grid.min.css"
+   * - "components/header.min.css"
+   * - "components/icon.min.css"
+   * - "components/image.min.css"
+   * - "components/input.min.css"
+   * - "components/item.min.css"
+   * - "components/label.min.css"
+   * - "components/list.min.css"
+   * - "components/loader.min.css"
+   * - "components/menu.min.css"
+   * - "components/message.min.css"
+   * - "components/modal.min.css"
+   * - "components/nag.min.css"
+   * - "components/placeholder.min.css"
+   * - "components/popup.min.css"
+   * - "components/progress.min.css"
+   * - "components/rail.min.css"
+   * - "components/rating.min.css"
+   * - "components/reset.min.css"
+   * - "components/reveal.min.css"
+   * - "components/search.min.css"
+   * - "components/segment.min.css"
+   * - "components/shape.min.css"
+   * - "components/sidebar.min.css"
+   * - "components/site.min.css"
+   * - "components/slider.min.css"
+   * - "components/statistic.min.css"
+   * - "components/step.min.css"
+   * - "components/sticky.min.css"
+   * - "components/tab.min.css"
+   * - "components/table.min.css"
+   * - "components/text.min.css"
+   * - "components/toast.min.css"
+   * - "components/transition.min.css"
+   * - "components/accordion.min.js"
+   * - "components/api.min.js"
+   * - "components/calendar.min.js"
+   * - "components/checkbox.min.js"
+   * - "components/dimmer.min.js"
+   * - "components/dropdown.min.js"
+   * - "components/embed.min.js"
+   * - "components/flyout.min.js"
+   * - "components/form.min.js"
+   * - "components/modal.min.js"
+   * - "components/nag.min.js"
+   * - "components/popup.min.js"
+   * - "components/progress.min.js"
+   * - "components/rating.min.js"
+   * - "components/search.min.js"
+   * - "components/shape.min.js"
+   * - "components/sidebar.min.js"
+   * - "components/site.min.js"
+   * - "components/slider.min.js"
+   * - "components/state.min.js"
+   * - "components/sticky.min.js"
+   * - "components/tab.min.js"
+   * - "components/toast.min.js"
+   * - "components/transition.min.js"
+   * - "components/visibility.min.js"
+   * - "themes/default/assets/fonts/Lato-Bold.woff"
+   * - "themes/default/assets/fonts/Lato-BoldItalic.woff"
+   * - "themes/default/assets/fonts/Lato-Italic.woff"
+   * - "themes/default/assets/fonts/Lato-Regular.woff"
+   * - "themes/default/assets/fonts/LatoLatin-Bold.woff"
+   * - "themes/default/assets/fonts/LatoLatin-BoldItalic.woff"
+   * - "themes/default/assets/fonts/LatoLatin-Italic.woff"
+   * - "themes/default/assets/fonts/LatoLatin-Regular.woff"
+   * - "themes/default/assets/fonts/brand-icons.woff"
+   * - "themes/default/assets/fonts/icons.woff"
+   * - "themes/default/assets/fonts/outline-icons.woff"
+   * - "themes/default/assets/fonts/Lato-Bold.woff2"
+   * - "themes/default/assets/fonts/Lato-BoldItalic.woff2"
+   * - "themes/default/assets/fonts/Lato-Italic.woff2"
+   * - "themes/default/assets/fonts/Lato-Regular.woff2"
+   * - "themes/default/assets/fonts/LatoLatin-Bold.woff2"
+   * - "themes/default/assets/fonts/LatoLatin-BoldItalic.woff2"
+   * - "themes/default/assets/fonts/LatoLatin-Italic.woff2"
+   * - "themes/default/assets/fonts/LatoLatin-Regular.woff2"
+   * - "themes/default/assets/fonts/brand-icons.woff2"
+   * - "themes/default/assets/fonts/icons.woff2"
+   * - "themes/default/assets/fonts/outline-icons.woff2"
+   *
+   */
+  static func resourceNamed(_ name: String) -> Data? {
+    switch name {
+
+      case "semantic.min.css":
+        return data_semantic_min_css
+
+      case "semantic.min.js":
+        return data_semantic_min_js
+
+      case "components/accordion.min.css":
+        return data_accordion_min_css
+
+      case "components/ad.min.css":
+        return data_ad_min_css
+
+      case "components/breadcrumb.min.css":
+        return data_breadcrumb_min_css
+
+      case "components/button.min.css":
+        return data_button_min_css
+
+      case "components/calendar.min.css":
+        return data_calendar_min_css
+
+      case "components/card.min.css":
+        return data_card_min_css
+
+      case "components/checkbox.min.css":
+        return data_checkbox_min_css
+
+      case "components/comment.min.css":
+        return data_comment_min_css
+
+      case "components/container.min.css":
+        return data_container_min_css
+
+      case "components/dimmer.min.css":
+        return data_dimmer_min_css
+
+      case "components/divider.min.css":
+        return data_divider_min_css
+
+      case "components/dropdown.min.css":
+        return data_dropdown_min_css
+
+      case "components/embed.min.css":
+        return data_embed_min_css
+
+      case "components/emoji.min.css":
+        return data_emoji_min_css
+
+      case "components/feed.min.css":
+        return data_feed_min_css
+
+      case "components/flag.min.css":
+        return data_flag_min_css
+
+      case "components/flyout.min.css":
+        return data_flyout_min_css
+
+      case "components/form.min.css":
+        return data_form_min_css
+
+      case "components/grid.min.css":
+        return data_grid_min_css
+
+      case "components/header.min.css":
+        return data_header_min_css
+
+      case "components/icon.min.css":
+        return data_icon_min_css
+
+      case "components/image.min.css":
+        return data_image_min_css
+
+      case "components/input.min.css":
+        return data_input_min_css
+
+      case "components/item.min.css":
+        return data_item_min_css
+
+      case "components/label.min.css":
+        return data_label_min_css
+
+      case "components/list.min.css":
+        return data_list_min_css
+
+      case "components/loader.min.css":
+        return data_loader_min_css
+
+      case "components/menu.min.css":
+        return data_menu_min_css
+
+      case "components/message.min.css":
+        return data_message_min_css
+
+      case "components/modal.min.css":
+        return data_modal_min_css
+
+      case "components/nag.min.css":
+        return data_nag_min_css
+
+      case "components/placeholder.min.css":
+        return data_placeholder_min_css
+
+      case "components/popup.min.css":
+        return data_popup_min_css
+
+      case "components/progress.min.css":
+        return data_progress_min_css
+
+      case "components/rail.min.css":
+        return data_rail_min_css
+
+      case "components/rating.min.css":
+        return data_rating_min_css
+
+      case "components/reset.min.css":
+        return data_reset_min_css
+
+      case "components/reveal.min.css":
+        return data_reveal_min_css
+
+      case "components/search.min.css":
+        return data_search_min_css
+
+      case "components/segment.min.css":
+        return data_segment_min_css
+
+      case "components/shape.min.css":
+        return data_shape_min_css
+
+      case "components/sidebar.min.css":
+        return data_sidebar_min_css
+
+      case "components/site.min.css":
+        return data_site_min_css
+
+      case "components/slider.min.css":
+        return data_slider_min_css
+
+      case "components/statistic.min.css":
+        return data_statistic_min_css
+
+      case "components/step.min.css":
+        return data_step_min_css
+
+      case "components/sticky.min.css":
+        return data_sticky_min_css
+
+      case "components/tab.min.css":
+        return data_tab_min_css
+
+      case "components/table.min.css":
+        return data_table_min_css
+
+      case "components/text.min.css":
+        return data_text_min_css
+
+      case "components/toast.min.css":
+        return data_toast_min_css
+
+      case "components/transition.min.css":
+        return data_transition_min_css
+
+      case "components/accordion.min.js":
+        return data_accordion_min_js
+
+      case "components/api.min.js":
+        return data_api_min_js
+
+      case "components/calendar.min.js":
+        return data_calendar_min_js
+
+      case "components/checkbox.min.js":
+        return data_checkbox_min_js
+
+      case "components/dimmer.min.js":
+        return data_dimmer_min_js
+
+      case "components/dropdown.min.js":
+        return data_dropdown_min_js
+
+      case "components/embed.min.js":
+        return data_embed_min_js
+
+      case "components/flyout.min.js":
+        return data_flyout_min_js
+
+      case "components/form.min.js":
+        return data_form_min_js
+
+      case "components/modal.min.js":
+        return data_modal_min_js
+
+      case "components/nag.min.js":
+        return data_nag_min_js
+
+      case "components/popup.min.js":
+        return data_popup_min_js
+
+      case "components/progress.min.js":
+        return data_progress_min_js
+
+      case "components/rating.min.js":
+        return data_rating_min_js
+
+      case "components/search.min.js":
+        return data_search_min_js
+
+      case "components/shape.min.js":
+        return data_shape_min_js
+
+      case "components/sidebar.min.js":
+        return data_sidebar_min_js
+
+      case "components/site.min.js":
+        return data_site_min_js
+
+      case "components/slider.min.js":
+        return data_slider_min_js
+
+      case "components/state.min.js":
+        return data_state_min_js
+
+      case "components/sticky.min.js":
+        return data_sticky_min_js
+
+      case "components/tab.min.js":
+        return data_tab_min_js
+
+      case "components/toast.min.js":
+        return data_toast_min_js
+
+      case "components/transition.min.js":
+        return data_transition_min_js
+
+      case "components/visibility.min.js":
+        return data_visibility_min_js
+
+      case "themes/default/assets/fonts/Lato-Bold.woff":
+        return data_Lato_Bold_woff
+
+      case "themes/default/assets/fonts/Lato-BoldItalic.woff":
+        return data_Lato_BoldItalic_woff
+
+      case "themes/default/assets/fonts/Lato-Italic.woff":
+        return data_Lato_Italic_woff
+
+      case "themes/default/assets/fonts/Lato-Regular.woff":
+        return data_Lato_Regular_woff
+
+      case "themes/default/assets/fonts/LatoLatin-Bold.woff":
+        return data_LatoLatin_Bold_woff
+
+      case "themes/default/assets/fonts/LatoLatin-BoldItalic.woff":
+        return data_LatoLatin_BoldItalic_woff
+
+      case "themes/default/assets/fonts/LatoLatin-Italic.woff":
+        return data_LatoLatin_Italic_woff
+
+      case "themes/default/assets/fonts/LatoLatin-Regular.woff":
+        return data_LatoLatin_Regular_woff
+
+      case "themes/default/assets/fonts/brand-icons.woff":
+        return data_brand_icons_woff
+
+      case "themes/default/assets/fonts/icons.woff":
+        return data_icons_woff
+
+      case "themes/default/assets/fonts/outline-icons.woff":
+        return data_outline_icons_woff
+
+      case "themes/default/assets/fonts/Lato-Bold.woff2":
+        return data_Lato_Bold_woff2
+
+      case "themes/default/assets/fonts/Lato-BoldItalic.woff2":
+        return data_Lato_BoldItalic_woff2
+
+      case "themes/default/assets/fonts/Lato-Italic.woff2":
+        return data_Lato_Italic_woff2
+
+      case "themes/default/assets/fonts/Lato-Regular.woff2":
+        return data_Lato_Regular_woff2
+
+      case "themes/default/assets/fonts/LatoLatin-Bold.woff2":
+        return data_LatoLatin_Bold_woff2
+
+      case "themes/default/assets/fonts/LatoLatin-BoldItalic.woff2":
+        return data_LatoLatin_BoldItalic_woff2
+
+      case "themes/default/assets/fonts/LatoLatin-Italic.woff2":
+        return data_LatoLatin_Italic_woff2
+
+      case "themes/default/assets/fonts/LatoLatin-Regular.woff2":
+        return data_LatoLatin_Regular_woff2
+
+      case "themes/default/assets/fonts/brand-icons.woff2":
+        return data_brand_icons_woff2
+
+      case "themes/default/assets/fonts/icons.woff2":
+        return data_icons_woff2
+
+      case "themes/default/assets/fonts/outline-icons.woff2":
+        return data_outline_icons_woff2
+
+      default: return nil
+    }
+  }
+}
